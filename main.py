7fd907c458d0e4b14db79b7489171a2dc0e5a5bf
EnaKamei <161276039+EnaKamei@users.noreply.github.com>
2024-05-09 13:18:54 +0900

    fizzbuzz２問目

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# This is a sample Python script.
-
+# This is a sample Python script
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -21,52 +20,55 @@ def ex_input1():
 
 #3-1
 def ex_input31():
-    num1=int(input("３−１　３を加えて表示"))
+    num1 = int(input("３−１　３を加えて表示"))
     print(num1+3)
 
 #3-2
 def ex_input32():
-    num2=int(input("入力３−２　３倍にして表示"))
-    print(num2*3)
+    num2 = int(input("入力３−２　３倍にして表示"))
+    print(num2 * 3)
 
 #4-1
 def ex_condition():
     num41=int(input("整数を入力してください"))
-    if num41>0:
+    if num41 > 0:
         print("positive")
 
 #4-2
 def ex_condition2():
     num42=int(input("整数を入力してください"))
-    if num42>0:
+    if num42 > 0 :
         print("positive")
-    elif num42<0:
+    elif num42 < 0:
         print("negative")
-    elif num42==0:
+    elif num42 == 0:
         print("zero")
 
 #No.1 fizzbuzz 1-1
 def ex_fizzbuzz():
     value=int(input("整数を入力fizzbuzz"))
-    if value%3==0 and value%5==0:
+    if value % 3 == 0 and value % 5 == 0:
         print("fizzbuzz")
-    elif value%5==0:
+    elif value % 5 == 0:
         print("buzz")
-    elif value % 3==0:
+    elif value % 3 == 0:
         print("fizz")
 
 #No.1 fizzbuzz 1-2
 def ex_fizzbuzz2():
     value=int(input("fizzbuzz2の整数を入力"))
-    for x in range(1,value):
-        if x%3==0 and x%5==0:
-            print("fizzbuzz")
-        elif x%3==0:
-            print("fizz")
-        elif x%5==0:
-            print("buzz")
-        else :
-            print(x)
+    if value > 0:
+        for x in range(1,value):
+            if x % 3 == 0 and x % 5 == 0:
+                print("fizzbuzz")
+            elif x % 3 == 0:
+                print("fizz")
+            elif x % 5 == 0:
+                print("buzz")
+            else :
+                print(x)
+    else :
+        print("正の整数を入力してください。")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -81,3 +83,4 @@ if __name__ == '__main__':
     ex_fizzbuzz()
     ex_fizzbuzz2()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Press the green button in the gutter to run the script.
