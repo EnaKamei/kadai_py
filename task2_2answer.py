325ea2bc72f88a35b8fe8f0391dde9d535d49d24
EnaKamei <161276039+EnaKamei@users.noreply.github.com>
2024-05-10 18:21:29 +0900

    課題2　解答と解説（再）(#2)

--- task2_2answer.py
+++ task2_2answer.py
@@ -3,33 +3,50 @@ def fizzbuzz_sub(x):
     :return: 計算結果
     """
     result = ""
-    if x % 3 == 0 and x % 5 == 0:
-        result = "fizzbuzz"
-    elif x % 3 == 0:
-        result = "fizz"
-    elif x % 5 == 0:
-        result = "buzz"
-    else:
+    # 3の倍数だったらfizzを追加
+    if x % 3 == 0:
+        result += "fizz"
+
+    # 5の倍数だったらbuzzを追加
+    if x % 5 == 0:
+        result += "buzz"
+
+    # どちらにも該当しなかった場合は数値
+    if result == "":
         result = str(x)
-    
+
     return result
+
+
+def input_data():
+    value = -1
+
+    while not value > 0:
+        try:
+            value = int(input("正の値を入力してください・・・"))
+        except ValueError as e:
+            continue
+
+    return value
+
 
 # pattern1
 def fizzbuzz202():
-    value = input("正の値を入力してください・・・")
-    try:
-        value = int(value)
-    except:
-        print("自然数を入力してください。")
-        fizzbuzz202()
-    if value < 0:
-        print("正の値を入れてください。")
-        fizzbuzz202()
+    input_value = input_data()
 
-    for x in range(1,value+1):
-        print(fizzbuzz_sub(x))
+    for x in range(1, input_value + 1):
+        result = fizzbuzz_sub(x)
+        print(result)
+
+
+def fizzbuzz201():
+    input_value = input_data()
+
+    print(fizzbuzz_sub(input_value))
+
 
 if __name__ == "__main__":
-    fizzbuzz202()
+    # fizzbuzz202()
+    fizzbuzz201()
 
 # comand + option + L で最後フォーマット整えること。
